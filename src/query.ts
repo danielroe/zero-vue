747b0e0e0b8b10a547a6fe908e5fac4db054da9d
Gerben Mulder <github.undergo381@passmail.net>
2025-01-19 13:54:17 +0100

    feat: update view and query to match 0.11

--- src/query.ts
+++ src/query.ts
@@ -4,12 +4,21 @@ import type { AdvancedQuery, Query, QueryType, Smash, TableSchema } from '@rocic
 import type { ComputedRef, MaybeRefOrGetter } from 'vue'
 import { computed, getCurrentInstance, isRef, onUnmounted, shallowRef, toValue, watch } from 'vue'
 
-import { vueViewFactory } from './view'
+import { type ResultType, vueViewFactory } from './view'
+
+export type QueryResultDetails = Readonly<{
+  type: ResultType
+}>
+
+export type QueryResult<TReturn extends QueryType> = readonly [
+  ComputedRef<Smash<TReturn>>,
+  ComputedRef<QueryResultDetails>,
+]
 
 export function useQuery<
   TSchema extends TableSchema,
   TReturn extends QueryType,
->(_query: MaybeRefOrGetter<Query<TSchema, TReturn>>): ComputedRef<Smash<TReturn>> {
+>(_query: MaybeRefOrGetter<Query<TSchema, TReturn>>): QueryResult<TReturn> {
   const query = toValue(_query) as AdvancedQuery<TSchema, TReturn>
   const view = shallowRef(query.materialize(vueViewFactory))
 
@@ -24,5 +33,8 @@ export function useQuery<
     onUnmounted(() => view.value.destroy())
   }
 
-  return computed(() => view.value.data)
+  return [
+    computed(() => view.value.data),
+    computed(() => ({ type: view.value.resultType })),
+  ] as const
 }
--- src/view.ts
+++ src/view.ts
@@ -1,8 +1,10 @@
 // based on https://github.com/rocicorp/mono/tree/main/packages/zero-solid
 
-import type { Change, Entry, Format, Input, Output, Query, QueryType, Smash, TableSchema, View } from '@rocicorp/zero/advanced'
+import type { Change, Entry, Format, Input, Output, Query, QueryType, Smash, TableSchema, View, ViewFactory } from '@rocicorp/zero/advanced'
 import { applyChange } from '@rocicorp/zero/advanced'
 import { reactive } from 'vue'
+
+export type ResultType = 'complete' | 'unknown'
 
 export class VueView<V extends View> implements Output {
   readonly #input: Input
@@ -12,17 +14,22 @@ export class VueView<V extends View> implements Output {
   // Synthetic "root" entry that has a single "" relationship, so that we can
   // treat all changes, including the root change, generically.
   readonly #root: Entry
+  readonly #resultType: { resultType: ResultType }
 
   constructor(
     input: Input,
     format: Format = { singular: false, relationships: {} },
     onDestroy: () => void = () => {},
+    queryComplete: true | Promise<true> = true,
   ) {
     this.#input = input
     this.#format = format
     this.#onDestroy = onDestroy
     this.#root = reactive({
       '': format.singular ? undefined : [],
+    })
+    this.#resultType = reactive({
+      resultType: queryComplete === true ? 'complete' : 'unknown',
     })
     input.setOutput(this)
 
@@ -35,10 +42,19 @@ export class VueView<V extends View> implements Output {
         this.#format,
       )
     }
+    if (queryComplete !== true) {
+      void queryComplete.then(() => {
+        this.#resultType.resultType = 'complete'
+      })
+    }
   }
 
   get data() {
     return this.#root[''] as V
+  }
+
+  get resultType() {
+    return this.#resultType.resultType
   }
 
   destroy() {
@@ -64,8 +80,12 @@ export function vueViewFactory<
   input: Input,
   format: Format,
   onDestroy: () => void,
+  _onTransactionCommit: (cb: () => void) => void,
+  queryComplete: true | Promise<true>,
 ): VueView<Smash<TReturn>> {
-  const v = new VueView<Smash<TReturn>>(input, format, onDestroy)
+  const v = new VueView<Smash<TReturn>>(input, format, onDestroy, queryComplete)
 
   return v
 }
+
+vueViewFactory satisfies ViewFactory<TableSchema, QueryType, unknown>
