653be5844cecee2ffbd089d33129f377fa6f33f6
Gerben Mulder <gerbuuun@pm.me>
2026-07-08 14:51:08 +0200

    revert: remove Nuxt fixture source alias

--- test/fixtures/nuxt/nuxt.config.ts
+++ test/fixtures/nuxt/nuxt.config.ts
@@ -24,11 +24,6 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-05-25',
   vite: {
-    resolve: {
-      alias: {
-        'zero-vue': fileURLToPath(new URL('../../../src/index.ts', import.meta.url)),
-      },
-    },
     optimizeDeps: {
       esbuildOptions: {
         target: 'es2022',
